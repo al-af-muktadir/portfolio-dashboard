f6203fcd9aab6621de817b9c97d836242b1b3a28
al-af-muktadir <al.afmuktadir.28@gmail.com>
2025-05-21 00:01:49 +0600

    lastone2

--- src/redux/apis/baseApi/BaseApi.ts
+++ src/redux/apis/baseApi/BaseApi.ts
@@ -3,7 +3,7 @@ import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 export const baseApi = createApi({
   reducerPath: "baseApi",
   baseQuery: fetchBaseQuery({
-    baseUrl: "https://portfolio-backend-seven-mocha.vercel.app/api ",
+    baseUrl: "https://portfolio-backend-seven-mocha.vercel.app/api",
     credentials: "include",
   }),
 
